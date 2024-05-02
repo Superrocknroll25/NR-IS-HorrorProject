f7f33318ddb42d7b02538caa350e9a4d0e4f4641
Superrocknroll25 <122689180+Superrocknroll25@users.noreply.github.com>
2024-05-02 19:40:16 -0400

    Hi

--- IS, NR - HorrorProject/Assets/move.cs
+++ IS, NR - HorrorProject/Assets/move.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    public GameObject TestPlayer;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,21 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            TestPlayer.transform.position = new Vector3(TestPlayer.transform.position.x + speed, TestPlayer.transform.position.y, TestPlayer.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            TestPlayer.transform.position = new Vector3(TestPlayer.transform.position.x - speed, TestPlayer.transform.position.y, TestPlayer.transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            TestPlayer.transform.position = new Vector3(TestPlayer.transform.position.x, TestPlayer.transform.position.y, TestPlayer.transform.position.z + speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            TestPlayer.transform.position = new Vector3(TestPlayer.transform.position.x, TestPlayer.transform.position.y, TestPlayer.transform.position.z - speed);
+        }
     }
 }

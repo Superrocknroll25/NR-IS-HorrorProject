612eec0a70d5a6b607e677d1370641765ffecfec
Glassis76019 <glassis76019@gmail.com>
2024-05-02 21:03:45 -0400

    Fixed Player Movement, camera, And added Dark enviroment with Flash light
    Dark enviroment, and light

--- IS, NR - HorrorProject/Assets/PlayerMovement.cs
+++ IS, NR - HorrorProject/Assets/PlayerMovement.cs
@@ -2,26 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-
     public CharacterController controller;
-    Vector3 velocity;
-    public float speed = 12f;
-    public float gravity = -9.81f;
 
-    // Update is called once per frame
+    public float speed = 12;
+    public float gravity = -9.81f;
+    public float Jumpheight = 3f;
+
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask GroundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    
+
+
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        
+            isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, GroundMask);
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
 
-        velocity.y += gravity * Time.deltaTime;
 
-        controller.Move(move * speed * Time.deltaTime);              
 
-        controller.Move(velocity * Time.deltaTime);
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(Jumpheight * -2 * gravity);
+
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
+        }
     }
-}
+

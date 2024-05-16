8041afab7ada6426ff3bf6496621eaa7dd79cda5
Glassis76019 <glassis76019@gmail.com>
2024-05-16 17:34:11 -0400

    Revert "hi"
    This reverts commit 3f5aadfc6c17dc11cc8dec98c91c7066261b1841.

--- IS, NR - HorrorProject/Assets/Crosshair.cs
+++ IS, NR - HorrorProject/Assets/Crosshair.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
+    int layerMask = 1 << 10;
     RaycastHit hit;
+    public string HitWhat;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +16,14 @@ public class Crosshair : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3f, layerMask))
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3f, layerMask))
         {
             if (Input.GetMouseButtonDown(0))
             {
                 hit.transform.name = HitWhat;
                 Debug.Log(HitWhat);
             }
-        }*/
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Physics.Raycast(Vector3.zero, Vector3.forward, 3f);
         }
+        
     }
 }

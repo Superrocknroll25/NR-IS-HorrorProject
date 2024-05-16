3f5aadfc6c17dc11cc8dec98c91c7066261b1841
Superrocknroll25 <122689180+Superrocknroll25@users.noreply.github.com>
2024-05-16 17:30:56 -0400

    hi

--- IS, NR - HorrorProject/Assets/Crosshair.cs
+++ IS, NR - HorrorProject/Assets/Crosshair.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
-    int layerMask = 1 << 10;
     RaycastHit hit;
-    public string HitWhat;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +14,18 @@ public class Crosshair : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3f, layerMask))
+        /*if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3f, layerMask))
         {
             if (Input.GetMouseButtonDown(0))
             {
                 hit.transform.name = HitWhat;
                 Debug.Log(HitWhat);
             }
+        }*/
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Physics.Raycast(Vector3.zero, Vector3.forward, 3f);
         }
-        
     }
 }

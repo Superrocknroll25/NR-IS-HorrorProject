738e1627593a02cdc156295ceb268c2d4dc5cf0e
Glassis76019 <glassis76019@gmail.com>
2024-05-16 16:53:52 -0400

    s
    s

--- IS, NR - HorrorProject/Assets/Scripts/RoomManager.cs
+++ IS, NR - HorrorProject/Assets/Scripts/RoomManager.cs
@@ -2,23 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.SceneManagement;
 
-public class ConnectToServer : MonoBehaviourPunCallbacks
+public class RoomManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Connecting...");
+
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected to Server");
+
         PhotonNetwork.JoinLobby();
     }
 
+
     public override void OnJoinedLobby()
     {
-        SceneManager.LoadScene("Lobby");
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("Test", null, null);
+
+        Debug.Log("Connected To Room");
+
     }
 }

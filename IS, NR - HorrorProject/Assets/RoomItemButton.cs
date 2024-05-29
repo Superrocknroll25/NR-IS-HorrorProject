385b9e682857f167b0ff1ce27f7cbfde53d9d735
Glassis76019 <glassis76019@gmail.com>
2024-05-29 17:24:14 -0400

    s
    s

--- IS, NR - HorrorProject/Assets/RoomItemButton.cs
+++ IS, NR - HorrorProject/Assets/RoomItemButton.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class RoomItemButton : MonoBehaviour
 {
     public string RoomName;
+    private GameObject JoinGameScreen;
+
+    private void Start()
+    {
+        JoinGameScreen = GameObject.Find("Join Game Screen");
+    }
 
     public void OnButtonPressed()
     {
         RoomList.Instance.JoinRoomsByName(RoomName);
+
+        JoinGameScreen.SetActive(false);
     }
 }
 

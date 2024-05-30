4d711af1ae43f8f859e221900b7deed31eb33c6f
Glassis76019 <glassis76019@gmail.com>
2024-05-30 18:09:06 -0400

    s
    s

---
+++ IS, NR - HorrorProject/Assets/Scripts/CrossHair.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour
+{
+    public Camera RaycastCam;
+
+    public float range = 4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootRaycast();
+        }
+    }
+
+    private void ShootRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(RaycastCam.transform.position, RaycastCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}

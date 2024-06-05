c82f8943ede38bc1b9e7738c40054d1a75812d11
Glassis76019 <glassis76019@gmail.com>
2024-06-04 20:16:39 -0400

    Made a Slider that changes mouse sensitivity, Exiting room but still problems with it

--- IS, NR - HorrorProject/Assets/PauseMenuScript.cs
+++ IS, NR - HorrorProject/Assets/PauseMenuScript.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class PauseMenuScript : MonoBehaviour
+public class PauseMenuScript : MonoBehaviourPunCallbacks
 {
+    private ConnecterOfPreFabToScene PlayerPrefabConnecter;
+
+    [SerializeField] Slider Slider;
+
+    [SerializeField] MouseLook MouseLookScript;
+
+    private void Start()
+    {
+        PlayerPrefabConnecter = GameObject.Find("PlayerPrefabConnecterToLobby").GetComponent<ConnecterOfPreFabToScene>();
+    }
 
     public void LeaveGame()
     {
+        PlayerPrefabConnecter.EnableLobby();
+
         PhotonNetwork.LeaveRoom();
     }
 
     public void UnLock()
     {
         Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void SliderValueChange()
+    {
+        MouseLookScript.mouseSensitivity = Slider.value;
     }
 }
